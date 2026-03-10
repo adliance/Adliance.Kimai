7979436201aef1273f378853c1f13b05e18cb76d
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2026-03-10 11:39:05 +0100

    Add support for KeyVault

--- src/Adliance.Kimai.Vacations/Program.cs
+++ src/Adliance.Kimai.Vacations/Program.cs
@@ -1,7 +1,14 @@
+using System.Reflection;
 using Adliance.Kimai.Vacations;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var keyVaultUri = builder.Configuration["KeyVault:Uri"];
+if (!string.IsNullOrWhiteSpace(keyVaultUri))
+{
+    builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
+}
 builder.Services.Configure<KimaiSettings>(builder.Configuration.GetSection("Kimai"));
 builder.Services.AddScoped<VacationICalService>();
 
@@ -9,7 +16,7 @@ var app = builder.Build();
 app.UseStatusCodePages();
 app.UseDeveloperExceptionPage();
 
-app.MapGet("/", () => Results.Content("Adliance Kimai Vacations", "text/plain"));
+app.MapGet("/", () => Results.Content($"Adliance Kimai Vacations v{typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion}", "text/plain"));
 
 app.MapGet("/ical", async (VacationICalService service) =>
 {
